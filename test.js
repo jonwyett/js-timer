3b2cfca1216f8cec56a3bd249e73de9d0439e003
jonwyett <jonwyett@hotmail.com>
2019-07-16 14:28:38 -0400

    better test

--- test.js
+++ test.js
@@ -1,44 +1,24 @@
 var timer = require('./timer');
 
 timer.start();
-/*
-for (var i=0; i<10; i++) {
-    for (var p=0; p<10; p++) {
-        console.log('-test-');
+
+var count = 0;
+
+for (var i=0; i<100; i++) {
+    for (var p=0; p<100; p++) {
+        console.log(count++);
     }
 }
 
 timer.lap('First Long Test');
 
-for (var i=0; i<10; i++) {
-    for (var p=0; p<100; p++) {
-        console.log('-test-');
+count = 0;
+for (var i=0; i<100; i++) {
+    for (var p=0; p<200; p++) {
+        console.log(count++);
     }
 }
 
 timer.lap('Second Long Test');
-*/
 
-setTimeout(function() {
-    timer.lap('750');
-    setTimeout(function() {
-        timer.lap('250');
-        console.log(timer.report());
-    },250);   
-}, 750);
-
-//console.log(JSON.stringify(timer.report()));
-
-/*
-var hrLast = process.hrtime();
-//var hrEnd = null;
-setTimeout(function(){
-    hrLast = process.hrtime(hrLast);
-    console.log(JSON.stringify(hrLast));
-    hrLast = process.hrtime();
-    setTimeout(function() {
-        hrLast = process.hrtime(hrLast);
-        console.log(JSON.stringify(hrLast));
-    }, 250);
-}, 750);
-*/
+console.log(timer.report());
